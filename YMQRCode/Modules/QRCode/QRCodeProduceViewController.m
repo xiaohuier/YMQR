06f7090bc471fe89541d5424542a8c5f75c71689
womendexinshijie <womendexinshijie@sina.com>
2017-02-08 15:18:57 +0800

    添加内容
    极光推送

--- YMQRCode/Modules/QRCode/QRCodeProduceViewController.m
+++ YMQRCode/Modules/QRCode/QRCodeProduceViewController.m
@@ -9,7 +9,7 @@
 #import "QRCodeProduceViewController.h"
 
 #import "UIImage+QRCode.h"
-
+#import "ShareView.h"
 #import "QRCodeImageStyleViewController.h"
 
 
@@ -165,10 +165,7 @@
 {
     QRCodeImageStyleViewController *styleView = [[QRCodeImageStyleViewController alloc]initWithQRCodeImage:self.qrCodeImage];
     
-    
 //    styleView.delegate = self;
-    
-    
     
     [self.navigationController pushViewController:styleView animated:NO];
     
